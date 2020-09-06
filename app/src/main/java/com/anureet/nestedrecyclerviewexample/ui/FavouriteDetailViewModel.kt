e93b28631481e748ac1277295db2d098ce77c6f7
anureet19 <anureet021999@gmail.com>
2020-09-06 13:46:55 +0530

    updated use of viewmodel

--- app/src/main/java/com/anureet/nestedrecyclerviewexample/ui/FavouriteDetailViewModel.kt
+++ app/src/main/java/com/anureet/nestedrecyclerviewexample/ui/FavouriteDetailViewModel.kt
@@ -8,37 +8,11 @@ import kotlinx.coroutines.launch
 
 class FavouriteDetailViewModel(application: Application): AndroidViewModel(application) {
     private val repo: FavouritesDetailRepository = FavouritesDetailRepository(application)
-    private val _itemId = MutableLiveData<Long>(0)
 
-    val itemId: LiveData<Long>
-        get() = _itemId
-
-    val item: LiveData<Favourites> = Transformations.switchMap(_itemId){ id ->
-        repo.getTask(id)
-    }
-
-    fun setTaskId(id: Long){
-        if(_itemId.value != id){
-            _itemId.value = id
-        }
-    }
 
     fun saveTask(data: Favourites){
         viewModelScope.launch {
-            if(_itemId.value == 0L){
-                _itemId.value = repo.insertTask(data)
-            }else{
-                repo.updateTask(data)
-            }
+                repo.insertTask(data)
         }
     }
-
-    fun deleteTask(){
-        viewModelScope.launch {
-            item.value?.let{
-                repo.deleteTask(it)
-            }
-        }
-    }
-
 }
