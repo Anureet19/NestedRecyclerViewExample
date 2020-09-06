f6cb997c73c726d3d79811dd672f386a05abd901
anureet19 <anureet021999@gmail.com>
2020-09-06 13:46:16 +0530

    removed update and delete queries

--- app/src/main/java/com/anureet/nestedrecyclerviewexample/data/FavouritesDetailDao.kt
+++ app/src/main/java/com/anureet/nestedrecyclerviewexample/data/FavouritesDetailDao.kt
@@ -5,16 +5,8 @@ import androidx.room.*
 
 @Dao
 interface FavouritesDetailDao{
-    @Query("SELECT * FROM favourites WHERE `id` = :id")
-    fun getData(id: Long): LiveData<Favourites>
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun insertData(task: Favourites): Long
-
-    @Update
-    suspend fun updateData(task: Favourites)
-
-    @Delete
-    suspend fun deleteData(task: Favourites)
 
 }
--- app/src/main/java/com/anureet/nestedrecyclerviewexample/data/FavouritesDetailRepository.kt
+++ app/src/main/java/com/anureet/nestedrecyclerviewexample/data/FavouritesDetailRepository.kt
@@ -6,19 +6,8 @@ import androidx.lifecycle.LiveData
 class FavouritesDetailRepository(context: Application) {
     private val favouritesDetailDao : FavouritesDetailDao = FavouritesDatabase.getDatabase(context).favouritesDetailDao()
 
-    fun getTask(id: Long): LiveData<Favourites> {
-        return favouritesDetailDao.getData(id)
-    }
-
-    suspend fun insertTask(item: Favourites): Long{
+    suspend fun insertTask(item: Favourites): Long {
         return favouritesDetailDao.insertData(item)
     }
-    suspend fun updateTask(item: Favourites){
-        favouritesDetailDao.updateData(item)
-    }
-    suspend fun deleteTask(item: Favourites){
-        favouritesDetailDao.deleteData(item)
-    }
-
 
 }
